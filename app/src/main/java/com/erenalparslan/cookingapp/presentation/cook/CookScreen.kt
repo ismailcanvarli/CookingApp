882d0ab617f09eae8ede84a6f1baa7894ffb2d5d
İsmail Can Varlı <icanvarli@outlook.com>
2024-05-27 15:59:43 +0300

    Added preview CookScreen.

--- app/src/main/java/com/erenalparslan/cookingapp/presentation/cook/CookScreen.kt
+++ app/src/main/java/com/erenalparslan/cookingapp/presentation/cook/CookScreen.kt
@@ -1,24 +1,17 @@
 package com.erenalparslan.cookingapp.presentation.cook
 
 import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.createSavedStateHandle
-import com.erenalparslan.cookingapp.data.remote.response.CookDto
-import com.erenalparslan.cookingapp.data.remote.response.CookDtoItem
-import kotlinx.coroutines.runBlocking
 
 @Composable
 fun CookScreen() {
@@ -46,4 +39,10 @@ fun CookScreen() {
         }
     }
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCookScreen() {
+    CookScreen()
 }
