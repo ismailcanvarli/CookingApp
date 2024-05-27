850cba1f3ca41d7ed46e5976239f33bb20b16e9c
İsmail Can Varlı <icanvarli@outlook.com>
2024-05-27 21:28:09 +0300

    Implemented changes in AddRecipeScreen. Fixed LazyColumn child items, centered image display, limited category input to specific options, resolved DropdownMenuItem issue, and ensured text fields retain their input.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -87,7 +87,7 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
     // Coil
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 
     // Extended Icons
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
--- app/src/main/java/com/erenalparslan/cookingapp/presentation/addRecipe/AddRecipeScreen.kt
+++ app/src/main/java/com/erenalparslan/cookingapp/presentation/addRecipe/AddRecipeScreen.kt
@@ -2,12 +2,20 @@
 
 package com.erenalparslan.cookingapp.presentation.addRecipe
 
+import android.net.Uri
 import android.os.Build
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,100 +29,191 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.erenalparslan.cookingapp.R
 
 @Composable
 fun AddRecipeScreen() {
     var recipeName by remember { mutableStateOf("") }
     var servingSize by remember { mutableStateOf("") }
     var category by remember { mutableStateOf("") }
-    var ingredient by remember { mutableStateOf("") }
-    var quantity by remember { mutableStateOf("") }
     var howTo by remember { mutableStateOf("") }
-
     var prepTime by remember { mutableStateOf("") }
     var cookTime by remember { mutableStateOf("") }
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+    // Malzemeler ve miktarları tutan liste
+    var ingredientsList by remember { mutableStateOf(listOf<Pair<String, String>>()) }
+
+    val photoPickerLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.PickVisualMedia(),
+            onResult = { uri -> selectedImageUri = uri })
 
     Surface(modifier = Modifier.fillMaxSize()) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
         ) {
-            Text(text = "Tarif Ekle")
 
-            // Image picker
-            Button(onClick = { /* TODO: Implement image picking */ }) {
-                Text("Görsel Seçiniz")
-            }
-
-            // "Tarif" title
-            Text(text = "Tarif")
-
-            // Recipe name input
-            OutlinedTextField(value = recipeName,
-                onValueChange = { recipeName = it },
-                label = { Text("Tarif İsmi") })
-
-            OutlinedTextField(value = servingSize,
-                onValueChange = {
-                    if (it.toIntOrNull() != null && it.toInt() > 0) servingSize = it
-                },
-                label = { Text("Kaç kişilik") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-            )
-
-
-            // Preparation time input
-            OutlinedTextField(value = prepTime,
-                onValueChange = { prepTime = it },
-                label = { Text("Hazırlık Süresi") })
-
-            // Cooking time input
-            OutlinedTextField(value = cookTime,
-                onValueChange = { cookTime = it },
-                label = { Text("Pişirme Süresi") })
-
-            // Category selection
-            OutlinedTextField(value = category,
-                onValueChange = { category = it },
-                label = { Text("Kategori") })
-
-            // "Malzemeler" title
-            Text(text = "Malzemeler")
-
-            // Ingredient and quantity input
-            Row {
-                OutlinedTextField(value = ingredient,
-                    onValueChange = { ingredient = it },
-                    label = { Text("Malzeme") })
-
-                //TODO buradaki miktar kısmının detayı öğrenilecek
-                OutlinedTextField(value = quantity,
-                    onValueChange = {
-                        if (it.toIntOrNull() != null && it.toInt() > 0) quantity = it
-                    },
-                    label = { Text("Miktar") },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            item {
+                Text(
+                    text = "Tarif Ekle", fontSize = 24.sp, modifier = Modifier
                 )
-            }
 
-            // "Nasıl Yapılır" title
-            Text(text = "Nasıl Yapılır")
+                // Seçilen görseli gösteren Box
+                selectedImageUri?.let { uri ->
+                    Box(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .size(350.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        if (selectedImageUri != null) {
+                            AsyncImage(
+                                model = selectedImageUri,
+                                contentDescription = null,
+                                modifier = Modifier.fillMaxSize(),
+                                contentScale = ContentScale.Crop
+                            )
+                        } else {
+                            // Placeholder görüntüsü
+                            Image(
+                                painter = painterResource(id = R.drawable.placeholder),
+                                contentDescription = "Placeholder Image",
+                                modifier = Modifier.fillMaxSize(),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+                }
 
-            // How to make it input
-            OutlinedTextField(value = howTo,
-                onValueChange = { howTo = it },
-                label = { Text("Nasıl Yapılır") })
+                Button(onClick = {
+                    photoPickerLauncher.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                    )
+                }) {
+                    Text("Görsel Seçiniz")
+                }
 
-            // Submit button
-            Button(onClick = { /* TODO: Implement submission */ }) {
-                Text("Tarif Ekle")
+                // "Tarif" title
+                Text(
+                    text = "Tarif", fontSize = 24.sp
+                )
+
+                // Recipe name input
+                OutlinedTextField(value = recipeName,
+                    onValueChange = { recipeName = it },
+                    label = { Text("Tarif İsmi") },
+                    modifier = Modifier.fillMaxWidth().padding(4.dp)
+                )
+
+                Row {
+                    // Kaç kişilik olacağını belirten input
+                    OutlinedTextField(value = servingSize,
+                        onValueChange = {
+                            if (it.toIntOrNull() != null && it.toInt() > 0) servingSize = it
+                        },
+                        label = { Text("Kaç kişilik") },
+                        modifier = Modifier.weight(1f).padding(horizontal = 4.dp),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+
+                    // Kategori seçimi
+                    OutlinedTextField(
+                        value = category, onValueChange = { category = it },
+                        label = { Text("Kategori") },
+                        modifier = Modifier.weight(1f).padding(horizontal = 4.dp),
+                    )
+                }
+
+                Row {
+                    // Preparation time input
+                    OutlinedTextField(value = prepTime,
+                        onValueChange = { prepTime = it },
+                        label = { Text("Hazırlık Süresi") },
+                        modifier = Modifier.weight(1f).padding(horizontal = 4.dp),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+
+                    // Cooking time input
+                    OutlinedTextField(value = cookTime,
+                        onValueChange = { cookTime = it },
+                        label = { Text("Pişirme Süresi") },
+                        modifier = Modifier.weight(1f).padding(horizontal = 4.dp),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                    )
+                }
+
+                // "Malzemeler" title
+                Text(
+                    text = "Malzemeler", fontSize = 24.sp
+                )
+
+                // Malzeme ve miktar girdileri
+                ingredientsList.forEachIndexed { index, (ingredient, quantity) ->
+                    var ingredientState by remember { mutableStateOf(ingredient) }
+                    var quantityState by remember { mutableStateOf(quantity) }
+
+                    Row {
+                        OutlinedTextField(value = ingredientState,
+                            onValueChange = { newValue ->
+                                ingredientState = newValue
+                                // Malzeme değerini güncelle
+                                ingredientsList = ingredientsList.toMutableList().also {
+                                    it[index] = ingredientState to it[index].second
+                                }
+                            },
+                            label = { Text("Malzeme") },
+                            modifier = Modifier.weight(1.5f).padding(horizontal = 4.dp)
+                        )
+
+                        OutlinedTextField(value = quantityState,
+                            onValueChange = { newValue ->
+                                quantityState = newValue
+                                // Miktar değerini güncelle
+                                ingredientsList = ingredientsList.toMutableList().also {
+                                    it[index] = it[index].first to quantityState
+                                }
+                            },
+                            label = { Text("Miktar") },
+                            modifier = Modifier.weight(0.5f).padding(horizontal = 4.dp),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                        )
+                    }
+                }
+
+                // Yeni malzeme ve miktar girdisi eklemek için buton
+                Button(onClick = {
+                    // Yeni bir malzeme ve miktar girdisi ekler
+                    ingredientsList = ingredientsList + ("" to "")
+                }) {
+                    Text("Malzeme Ekle")
+                }
+
+                // "Nasıl Yapılır" title
+                Text(text = "Nasıl Yapılır", fontSize = 24.sp)
+
+                // How to make it input
+                OutlinedTextField(value = howTo,
+                    onValueChange = { howTo = it },
+                    label = { Text("Nasıl Yapılır") },
+                    modifier = Modifier.fillMaxWidth().padding(4.dp)
+                )
+
+                // Submit button
+                Button(onClick = { /* TODO: Implement submission */ }) {
+                    Text("Tarif Ekle")
+                }
             }
         }
     }
 }
-
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
